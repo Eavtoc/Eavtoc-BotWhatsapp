a031c68d13187b1c18baeb1fc762700cffe113c6
Εαυτός <166735863+Eavtoc@users.noreply.github.com>
2025-02-17 19:38:47 +0700

    Update group-linkgc.js

--- plugins/group-linkgc.js
+++ plugins/group-linkgc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   try {
-    conn.reply(m.chat, `*Name Group* : ${await conn.getName(m.chat)}\n\n*Link Group* : https://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat) +`, m)
+    conn.reply(m.chat, `*Link Group:* ${await conn.getName(m.chat)}\n\nhttps://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat) + `\n\n${conn.user.name}`, m)
   } catch {
     conn.reply(m.chat, `Jadikan @${conn.user.jid.split('@')[0]} sebagai admin untuk menggunakan perintah ini!`, m, { mentions: [conn.user.jid] })
   }
@@ -10,6 +10,6 @@ handler.tags = ['group']
 handler.command = /^link(g(c)?ro?up)?$/i
 
 handler.group = true
-handler.admin = false
+handler.admin = true
 
 module.exports = handler
